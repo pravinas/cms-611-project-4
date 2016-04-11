84ef54b266d887db1e8964e2f87f7536611e0141
Danny Tang <data1013@mit.edu>
2016-04-11 13:43:16 -0400

    walls and springs

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+
+    public float springAmount;
+
+	// Use this for initialization
+	void Start () {
+        springAmount = 100f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    
+	}
+
+    void OnCollisionEnter(Collision collision) {
+
+        Rigidbody playerRB = collision.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 movement = new Vector3(0, springAmount, 0);
+
+        playerRB.AddForce(movement);
+    }
+}
