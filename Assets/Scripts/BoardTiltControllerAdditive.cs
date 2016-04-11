261012800938afc748c97f4c86a4cfd17814b694
unknown <davidwu@mit.edu>
2016-04-11 16:07:28 -0400

    Additively Add Control scheme working in additiveLoadControlTest - Base scene separated from controllers

---
+++ Assets/Scripts/BoardTiltControllerAdditive.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardTiltControllerAdditive : MonoBehaviour {
+	public float speed;
+	public static GameObject board;
+	private Rigidbody boardRigidbody;
+	// Use this for initialization
+	void Start () {
+		boardRigidbody = Statics.board.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	// FixedUpdate is called before performing 
+	// physics calculations.
+	void FixedUpdate () {
+		// Get keyboard input
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		boardRigidbody.angularVelocity = movement * speed;
+	}
+}
---
+++ Assets/Scripts/Statics.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Declares static variables for objects
+public class Statics : MonoBehaviour {
+	public GameObject groundRef;
+	public GameObject PlayerBallRef;
+
+	static public GameObject board;
+	static public GameObject PlayerBall;
+	// Use this for initialization
+	void Start () {
+		board = groundRef;
+		PlayerBall = PlayerBallRef;
+	}
+}
