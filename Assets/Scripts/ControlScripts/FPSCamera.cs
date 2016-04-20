2c5cd4138c9612186d733be3fd8843add2114457
William Wong <souleni5k@dhcp-18-189-62-129.dyn.mit.edu>
2016-04-20 11:00:38 -0400

    FPS controls foundation

---
+++ Assets/Scripts/ControlScripts/FPSCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCamera : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	public float turnSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	void Update() {
+	
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.up, -turnSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.up, turnSpeed * Time.deltaTime);
+		}
+	
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/ControlScripts/FPSControls1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSControls1 : MonoBehaviour {
+
+	public float speed;
+	public float turnSpeed;
+
+	private Rigidbody rb;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		speed = 10f;
+		turnSpeed = 50f;
+		rb = player.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			player.transform.Rotate (Vector3.up, -turnSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			player.transform.Rotate (Vector3.up, turnSpeed * Time.deltaTime);
+		}
+	}
+
+	// FixedUpdate is called before performing 
+	// physics calculations.
+	void FixedUpdate () {
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (0, 0, moveVertical);
+
+		rb.velocity = movement * speed;
+
+		player.transform.position = ;
+	}
+}
