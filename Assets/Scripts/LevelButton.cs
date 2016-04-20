66aa2f63c429d256fa409a228ffb0d1988a126d9
Pravina Samaratunga <pravinas@mit.edu>
2016-04-20 16:09:48 -0400

    Made the first level button pressable.

--- Assets/Scripts/LevelButton.cs
+++ Assets/Scripts/LevelButton.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class LevelButton : MonoBehaviour {
 
 	public string level;
+	private string baseScene = "BaseScene";
 
 	public void loadLevel(){
-		Application.LoadLevel (level);
+		Application.LoadLevel (baseScene);
+		Application.LoadLevelAdditive (level);
 	}
 }
