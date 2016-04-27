3cc0ae387c23f5c6c5b646f55e166e325866171a
unknown <davidwu@mit.edu>
2016-04-27 12:38:19 -0400

    fixed boardTilt script to not access rigidbody

--- Assets/Scripts/ControlScripts/BoardTiltController.cs
+++ Assets/Scripts/ControlScripts/BoardTiltController.cs
@@ -4,11 +4,13 @@ using System.Collections;
 // TODO: Change board tilt implementation to only rotate the board and not add velocity.
 public class BoardTiltController : MonoBehaviour {
 	public float speed;
-	private Rigidbody boardRigidbody;
+	private GameObject board;
 	// Use this for initialization
 	void Start () {
-		boardRigidbody = Statics.board.GetComponent<Rigidbody> ();
-		boardRigidbody.freezeRotation = false;
+		board = Statics.board;
+		speed = 10;
+		//boardRigidbody = Statics.board.GetComponent<Rigidbody> ();
+		//boardRigidbody.freezeRotation = false;
 	}
 
 	// Update is called once per frame
@@ -19,11 +21,18 @@ public class BoardTiltController : MonoBehaviour {
 	// physics calculations.
 	void FixedUpdate () {
 		// Get keyboard input
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+	
+		Transform boardTransform = board.transform;
+		if (Input.GetKey("left"))
+			boardTransform.Rotate(Vector3.forward *speed * Time.deltaTime, Space.World);
 
-		Vector3 movement = new Vector3 (moveVertical, 0, moveHorizontal);
+		if (Input.GetKey("right"))
+			boardTransform.Rotate(-Vector3.forward * speed * Time.deltaTime, Space.World);
 
-		boardRigidbody.angularVelocity = movement * speed;
+		if (Input.GetKey("up"))
+			boardTransform.Rotate(Vector3.left * speed * Time.deltaTime,Space.World);
+
+		if (Input.GetKey("down"))
+			boardTransform.Rotate(Vector3.right * speed * Time.deltaTime, Space.World);
 	}
 }
