336792e722fff3b531502893cee1e8d34801048a
Pravina Samaratunga <pravinas@mit.edu>
2016-05-02 14:58:47 -0400

    Keep trying to make things glow.

---
+++ Assets/Scripts/GlowImage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowImage : MonoBehaviour {
+	/*
+	private GameObject img;
+
+	// Use this for initialization
+	void Start () {
+		img = this.gameObject;
+		print (img.ToString);
+		img.transform.position.Set (img.transform.position.x, img.transform.position.y, - 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter(){
+		img.transform.position.Set (img.transform.position.x, img.transform.position.y, 1);
+	}
+
+	void OnMouseExit(){
+		img.transform.position.Set (img.transform.position.x, img.transform.position.y, - 1);
+	}*/
+}
