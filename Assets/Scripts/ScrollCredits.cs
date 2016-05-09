1d93c3df73e0a1cea5223702918074a69f6ea5b5
Pravina Samaratunga <pravinas@mit.edu>
2016-05-09 17:00:19 -0400

    Made scrolling credits

---
+++ Assets/Scripts/ScrollCredits.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollCredits : MonoBehaviour {
+	private bool isScroll;
+	public int speed = 20;
+
+	// Use this for initialization
+	void Start () {
+		this.isScroll = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isScroll) {
+			transform.Translate (new Vector3 (-speed * Time.deltaTime, 0));
+			if (transform.position.x < -7300) {
+				isScroll = false;
+			}
+		}
+	}
+
+	public void scrollLeft () {
+		transform.position.Set (7300, -575, 0);
+		this.isScroll = true;
+	}
+}
